96fef2badb7db07da021379655fe35539f54812f
Nitin Vohra <nitinvohra92@gmail.com>
2019-08-04 21:19:33 +0530

    todo app

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,26 +1,26 @@
 import React from "react";
 
 class Todolist extends React.Component {
-  //   constructor(props) {
-  //     super(props);
-  //     console.log(props);
-  //     this.state = {
-  //         name:this.props.title
-  //     }
-  //   }
-  render() {
-    return (
-      <div>
-        <li
-          className={this.props.todo.isDone ? "striked" : ""}
-          onClick={todo => this.props.handleClick(this.props.todo)}
-        >
-          {this.props.title}
-        </li>
-        <button onClick={todo => this.props.Deleted(this.props.todo)}>DELETE</button>
-      </div>
-    );
-  }
+    //   constructor(props) {
+    //     super(props);
+    //     console.log(props);
+    //     this.state = {
+    //         name:this.props.title
+    //     }
+    //   }
+    render() {
+        return (
+            <div>
+                <li
+                    className={this.props.todo.isDone ? "striked" : ""}
+                    onClick={todo => this.props.handleClick(this.props.todo)}
+                >
+                    {this.props.title}
+                </li>
+                <button onClick={todo => this.props.Deleted(this.props.todo)}>DELETE</button>
+            </div>
+        );
+    }
 }
 
 export default Todolist;
